b496506d941786d2602dccac7a838c31ff642fef
Sergey <serega.borovi4ok@gmail.com>
2023-09-15 13:42:16 +0300

    Add model Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,16 @@ class Topic(models.Model):
     def __str__(self):
         """Возвращает строковое представление модели"""
         return self.text
+
+
+class Entry(models.Model):
+    """Информация изученая по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
