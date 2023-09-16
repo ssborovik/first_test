fdae83fcc9ceb62d7dfb6f2815b04bc5185b9b23
Sergey <serega.borovi4ok@gmail.com>
2023-09-16 13:14:17 +0300

    Add HTML

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name= 'topic')
 ]
